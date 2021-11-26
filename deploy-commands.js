b034f2cfcca3aec0d2103617c541a6d25cd73a73
4season <junhoheo11@gmail.com>
2021-11-26 17:32:40 +0900

    Changes

---
+++ deploy-commands.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [];
+const commandFiles_L = fs.readdirSync('./commands/legacy/').filter(file => file.endsWith('.js'));
+const commandFiles_N = fs.readdirSync('./commands/new/').filter(file => file.endsWith('.js'));
+const commandFiles_T = fs.readdirSync('./commands/test-lab/').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles_L) {
+    const commandLegacy = require(`./commands/legacy/${file}`);
+    commands.push(commandLegacy.data.toJSON());
+}
+
+for (const file of commandFiles_N) {
+    const commandNew = require(`./commands/new/${file}`);
+    commands.push(commandNew.data.toJSON());
+}
+
+for (const file of commandFiles_T) {
+    const commandTestlab = require(`./commands/test-lab/${file}`);
+    commands.push(commandTestlab.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
--- mainNode.js
+++ mainNode.js
@@ -1,11 +1,47 @@
-const { Client, Intents } = require('discord.js');
-const token = process.env.TOKEN;
-//const { token } = require('./config.json');
+const fs = require('fs');
+const { Client, Intents, Collection } = require('discord.js');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection();
+const commandFiles_L = fs.readdirSync('./commands/legacy/').filter(file => file.endsWith('.js'));
+const commandFiles_N = fs.readdirSync('./commands/new/').filter(file => file.endsWith('.js'));
+const commandFiles_T = fs.readdirSync('./commands/test-lab/').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles_L) {
+    const commandLegacy = require(`./commands/legacy/${file}`);
+    client.commands.set(commandLegacy.data.name, commandLegacy);
+}
+
+for (const file of commandFiles_N) {
+    const commandNew = require(`./commands/new/${file}`);
+    client.commands.set(commandNew.data.name, commandNew);
+}
+
+for (const file of commandFiles_T) {
+    const commandTestlab = require(`./commands/test-lab/${file}`);
+    client.commands.set(commandTestlab.data.name, commandTestlab);
+}
+
 client.once('ready', () => {
-  console.log('Ready!');
+    console.log('client is ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
 });
 
 client.login(token);
+//"token": "process.env.token"
